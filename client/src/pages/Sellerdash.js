fc61c6e242434d13109b307d3b0d61836ab9534a
Jay <jaypja99@gmail.com>
2021-10-12 17:42:18 +0530

    a

--- client/src/pages/Sellerdash.js
+++ client/src/pages/Sellerdash.js
@@ -1,36 +1,39 @@
 import React from "react";
 import "../css/Sellerdash.css";
-import Dashnav  from "../components/Dashnav";
-import Dashtop  from "../components/Dashtop";
+import Dashnav from "../components/Dashnav";
+import Dashtop from "../components/Dashtop";
 const Sellerdash = () => {
   return (
     <div className="Sellerdash">
-        <div className='rowC'>
-            
-        <Dashnav/>
-        <div className='rowR'>
-        <Dashtop/>
-        <div className='rowC'>
-        <div className='row1'>
+      <div className='rowC'>
 
-        </div>
-        <div className='row2'>
-          
-        </div>
-        <div className='row3'>
-          
-        </div>
-        </div>
-        <div className='dash'>
-          
+        <Dashnav />
+        <div className='rowR'>
+          <Dashtop />
+          <div className='row1'>
+          </div>
+          <div className='rowC'>
+
+
+          </div>
+
+          <div className='row2'>
+
+          </div>
+          <div className='row3'>
+
           </div>
         </div>
+        <div className='dash'>
+
         </div>
-        
-        
-        
+      </div>
     </div>
-    );
-  };
-  
-  export default Sellerdash;
+
+
+
+
+  );
+};
+
+export default Sellerdash;
